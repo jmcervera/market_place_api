40ded8fd62f1363c9e672692b9571097099c3590
Juanma Cervera <juanmacervera@gmail.com>
2015-07-14 16:08:01 +0200

    Sets rspec and implements the api versioning constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
+require 'api_constraints'
+
+
 Rails.application.routes.draw do
 	#Api definition
-	namespace :api, defaults: { format: :json } do
+	namespace :api,
+						defaults: { format: :json },
+						constraints: { subdomain: 'api' },
+						path: '/' do
+
+		scope module: :v1,
+          constraints: ApiConstraints.new(version: 1, default: true) do
+
+		end
 
 	end
 end
