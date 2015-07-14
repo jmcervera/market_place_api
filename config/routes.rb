0dc5b3ac1446fd0bf8343c746788de2c8dc5c8da
Juanma Cervera <juanmacervera@gmail.com>
2015-07-14 18:08:31 +0200

    Adds destroy action to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 		scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
       # Our list of resources
-      resources :users, only: [:show, :create, :update]
+      resources :users, only: [:show, :create, :update, :destroy]
 
 		end
 
