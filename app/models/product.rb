3d17ee5faac26bda1124e596f30e461b266d26c1
Juanma Cervera <juanmacervera@gmail.com>
2015-07-18 08:34:58 +0200

    Associates products and orders with a placement model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
   belongs_to :user
 
+  has_many :placements
+  has_many :orders, through: :placements
+
   validates :title, :user, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 },
                     presence: true
