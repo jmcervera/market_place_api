8a1ff48874832fbd4c806efe91f1004cfb711193
Juanma Cervera <juanmacervera@gmail.com>
2015-07-18 09:14:24 +0200

    Adds index orders controller action nested int the users resource

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
       # Our list of resources
       resources :users, only: [:show, :create, :update, :destroy] do
       	resources :products, only: [:create, :update, :destroy]
+      	resources :orders, only: [:index]
       end
       resources :sessions, only: [:create, :destroy]
       resources :products, only: [:show, :index]
